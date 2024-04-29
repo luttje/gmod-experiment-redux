45c140d5cef1fbf713835c83c01c1bc2a1f3bef1
luttje <2738114+luttje@users.noreply.github.com>
2024-04-29 11:20:28 +0200

    change medic shirt name to identify category better

--- schema/items/armor/sh_medic_uniform.lua
+++ schema/items/armor/sh_medic_uniform.lua
@@ -1,9 +1,9 @@
 local ITEM = ITEM
 
 ITEM.price = 400
-ITEM.name = "Medic Uniform"
+ITEM.name = "Medic Shirt"
 ITEM.model = "models/Humans/Group03m/male_07.mdl"
-ITEM.description = "A medic uniform with a yellow insignia. Provides you with some bullet resistance."
+ITEM.description = "A medic uniform with a red cross on the arms. Provides you with some bullet resistance."
 ITEM.outfitCategory = "shirt"
 ITEM.width = 2
 ITEM.height = 1
