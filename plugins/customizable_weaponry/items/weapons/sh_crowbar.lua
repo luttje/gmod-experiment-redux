74b924a0c699f024030b4a3d8fbfd2c6b773955f
luttje <2738114+luttje@users.noreply.github.com>
2024-04-30 14:03:49 +0200

    Don't allow losing crowbar by transfering it

--- plugins/customizable_weaponry/items/weapons/sh_crowbar.lua
+++ plugins/customizable_weaponry/items/weapons/sh_crowbar.lua
@@ -20,3 +20,8 @@ ITEM.attachmentOffsetAngles = Angle(-57, 12.2, 0.7)
 ITEM.mergeIntoSwep = {
 	MeleeRange = 64,
 }
+
+function ITEM:CanTransfer(oldInventory, newInventory)
+    -- Only allow moving within the same inventory
+    return newInventory == nil
+end
