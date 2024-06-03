fdb69da9dfae4cf75a3b768fc38347493b770005
luttje <2738114+luttje@users.noreply.github.com>
2024-06-03 08:15:27 +0200

    Reduce Boltless Wanderer reward #110

--- schema/achievements/sh_boltless_wanderer.lua
+++ schema/achievements/sh_boltless_wanderer.lua
@@ -7,7 +7,7 @@ ACHIEVEMENT.foregroundImage = {
 	y = 7,
 	size = 32,
 }
-ACHIEVEMENT.reward = 5000
+ACHIEVEMENT.reward = 500
 ACHIEVEMENT.maximum = 1
 ACHIEVEMENT.description = "Experience the ultimate sacrifice by losing all your bolts to the chaos."
 
