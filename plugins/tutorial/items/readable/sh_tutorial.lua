e8b3c18decfbd7cb74b138bf638e7fdc7683382a
luttje <2738114+luttje@users.noreply.github.com>
2025-08-01 18:36:49 +0200

    Let player confirm before destroying tutorial

--- plugins/tutorial/items/readable/sh_tutorial.lua
+++ plugins/tutorial/items/readable/sh_tutorial.lua
@@ -9,13 +9,57 @@ ITEM.hideFrameTitleBar = true
 ITEM.hideFrameCloseButton = true
 
 function ITEM:GetText()
-    local html = Schema.util.GetHtml("tutorial.html")
+	local html = Schema.util.GetHtml("tutorial.html")
 
 	return html, true
 end
 
 function ITEM:GetFrameSize()
-    return ScrW(), ScrH()
+	return ScrW(), ScrH()
+end
+
+if (SERVER) then
+	util.AddNetworkString("expConfirmDestroy")
+	util.AddNetworkString("expConfirmedDestroy")
+
+	net.Receive("expConfirmedDestroy", function(len, client)
+		local itemID = net.ReadUInt(32)
+		local item = ix.item.instances[itemID]
+
+		if (not item) then
+			ix.util.SchemaErrorNoHalt("Attempted to destroy a lore item that doesn't exist!\n")
+			return
+		end
+
+		local inventory = client:GetCharacter():GetInventory()
+		inventory:Remove(itemID)
+
+		client:Notify("You have destroyed the item.")
+	end)
+end
+
+if (CLIENT) then
+	net.Receive("expConfirmDestroy", function()
+		local itemID = net.ReadUInt(32)
+		local item = ix.item.instances[itemID]
+
+		if (not item) then
+			ix.util.SchemaErrorNoHalt("Attempted to destroy a lore item that doesn't exist!\n")
+			return
+		end
+
+		Derma_Query(
+			"Are you sure you want to destroy this item? You will not be able to read it again.",
+			"Confirm Destroy Item",
+			L("yes"),
+			function()
+				net.Start("expConfirmedDestroy")
+				net.WriteUInt(itemID, 32)
+				net.SendToServer()
+			end,
+			L("no")
+		)
+	end)
 end
 
 ITEM.functions.destroy = {
@@ -23,7 +67,13 @@ ITEM.functions.destroy = {
 	tip = "Destroy the item.",
 	icon = "icon16/cross.png",
 	OnRun = function(item)
-		item.player:Notify("You have destroyed the item.")
+		local client = item.player
+
+		net.Start("expConfirmDestroy")
+		net.WriteUInt(item:GetID(), 32)
+		net.Send(client)
+
+		return false -- Prevents the item from being removed immediately
 	end,
 	OnCanRun = function(item)
 		if (CLIENT) then
