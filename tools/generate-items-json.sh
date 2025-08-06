a9a339bddc6dfdca889553e319086aafeafad9c2
luttje <2738114+luttje@users.noreply.github.com>
2025-08-06 14:34:54 +0200

    balancing web page: use only tailwindcss + implement shipment size + implement export + implement saving to localStorage

--- tools/generate-items-json.sh
+++ tools/generate-items-json.sh
@@ -17,7 +17,7 @@ DIRECTORIES=(plugins schema)
 echo "[" > "$OUTPUT_PATH"
 
 add_to_json() {
-    echo "  {\"name\": \"$1\", \"price\": $2, \"category\": \"$3\"}," >> "$OUTPUT_PATH"
+    echo "  {\"name\": \"$1\", \"shipmentSize\": $2, \"price\": $3, \"category\": \"$4\"}," >> "$OUTPUT_PATH"
 }
 
 for dir in "${DIRECTORIES[@]}"; do
@@ -25,21 +25,28 @@ for dir in "${DIRECTORIES[@]}"; do
         grep -r -H --include='*.lua' "ITEM.name =" "$dir" | while read -r name_line; do
             file=$(echo "$name_line" | cut -d: -f1)
             name=$(echo "$name_line" | sed -n "s/.*ITEM.name = \"\(.*\)\".*/\1/p")
+
             if [ -n "$name" ]; then
                 price=$(grep "ITEM.price = " "$file" | sed -n "s/.*ITEM.price = \([0-9]*\).*/\1/p")
+
                 if [ -n "$price" ]; then
                     category=$(grep "ITEM.category = " "$file" | sed -n "s/.*ITEM.category = \"\(.*\)\".*/\1/p")
                     weaponCategory=$(grep "ITEM.weaponCategory = " "$file" | sed -n "s/.*ITEM.weaponCategory = \"\(.*\)\".*/\1/p")
+                    shipmentSize=$(grep "ITEM.shipmentSize = " "$file" | sed -n "s/.*ITEM.shipmentSize = \([0-9]*\).*/\1/p")
 
                     if [ -z "$category" ]; then
                         if [ -n "$weaponCategory" ]; then
                             category="Weapon: $weaponCategory"
                         else
-                            category=$(echo "$file" | rev | cut -d'/' -f2 | rev)
+                            category=$(dirname "$file" | awk -F/ '{print $NF}')
                         fi
                     fi
 
-                    add_to_json "$name" "$price" "$category"
+                    if [ -z "$shipmentSize" ]; then
+                        shipmentSize=1
+                    fi
+
+                    add_to_json "$name" "$shipmentSize" "$price" "$category"
                 fi
             fi
         done
